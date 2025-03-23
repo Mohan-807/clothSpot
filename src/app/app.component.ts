05adf6a4d079a157c0a9af407ab12108e1e4aa00
Mohan <mohan8105072435@gmail.com>
2025-03-23 23:04:05 +0530

    project file changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  icon: string;
+  label: string;
+  route: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'clothSpot';
+  menuItems: MenuItem[] = [
+    { icon: 'home', label: 'Home', route: '/home' },
+    { icon: 'store', label: 'Shop', route: '/shop' },
+    { icon: 'article', label: 'Blog', route: '/blog' },
+    { icon: 'info', label: 'About', route: '/about' },
+    { icon: 'phone', label: 'Contact', route: '/contact' }
+  ];
 }
